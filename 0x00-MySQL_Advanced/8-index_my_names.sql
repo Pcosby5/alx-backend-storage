4f22216eb466d9135724143fccd4588cb58f5164
Pcosby5 <pcosby50@gmail.com>
2024-05-09 19:27:52 +0200

    8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,9 +1,3 @@
--- Drop the existing 'name' column
-ALTER TABLE names DROP COLUMN name;
-
--- Add the new 'name' column
-ALTER TABLE names ADD COLUMN name VARCHAR(255);
-
 -- Add a generated column to store the first letter of the name
 ALTER TABLE names ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
 
