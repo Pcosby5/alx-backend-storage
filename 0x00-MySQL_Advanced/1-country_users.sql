919dcec0d30f75effac96ce0b43ddd4835e1d934
Pcosby5 <pcosby50@gmail.com>
2024-05-09 21:57:51 +0200

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,8 @@
--- add a new attribute (country), using the ALTER TABLE
--- to alter the users table
-
-ALTER TABLE users
-ADD COLUMN country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
+-- creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
